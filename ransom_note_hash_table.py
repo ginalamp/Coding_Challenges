9525b4b7ae3fe262055c2f4f88b7332497a412b8
Gina <Gina>
2020-04-28 19:07:31 +0200

    Hackerrank, Dictionaries and Hashmaps: ransom note hash table problem -> fixed duplicate bug

--- ransom_note_hash_table.py
+++ ransom_note_hash_table.py
@@ -6,19 +6,27 @@ def main(magazine, ransom):
         @param magazine - an array of Strings (words available)
         @param ransom - an array of Strings (message wish to form)
 
-        @return "Yes" or "No" (depending on whether the ransom could be formed from the words in the magazine)
+        @output print "Yes" or "No" (depending on whether the ransom could be formed from the words in the magazine)
     '''
 
-    if set(ransom).issubset(magazine):
+    temp = []
+    for word in ransom:
+        if word in magazine:
+            magazine.remove(word)
+            temp.append(word)
+        else:
+            print("No")
+            break
+
+    if len(ransom) == len(temp):
         print("Yes")
-        return "Yes"
 
-    print("No")
-    return "No"
-
+    # NOTE: issubset does not cater for duplicates (e.g. ransom = ["a", "a", "b"] and magazine = ["a", "b"] will return True
+    # if set(ransom).issubset(magazine):
+    #    print("Yes")
 
 
 if __name__ == '__main__':
-    magazine = ["word2", "word3"]
-    ransom = ["word1", "word2"]
+    magazine = ["word2", "word1"]
+    ransom = ["word1", "word2", "word1"]
     main(magazine, ransom)

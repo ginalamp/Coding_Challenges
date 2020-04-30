c988bdf67f056c203a9ce9d4a3a322a04fc4d314
Gina <Gina>
2020-04-30 16:27:05 +0200

    Hackerrank, Warmup: Repeated Strings

---
+++ hacker_rank/warm_up/repeated_string.py
@@ -0,0 +1,33 @@
+'''
+    Given a string, a letter and an integer n, find and print the number that the given letter occurs in the first n letters of the infinite string.
+    The infinite string is defined as the given string that repeats an infinite amount of times.
+
+    e.g. 'aba' would become 'abaabaabaabaabaabaaba...' an "infinite amount of times. If one needed to find the number of a's in the first n=10 indices, the result would be 7.
+
+'''
+
+def main(s, n):
+    '''
+        @param s - a String
+        @param letter - character that should be counted
+        @param n - an integer indicating how many letters to search for in the "infinite String"
+
+        @return the number of times the specified letter repeats within the first n letters of repeated s
+    '''
+
+    # multiply s until you get to the point where adding another instance of s would be greater than n
+    result = s.count(letter) * (n // len(s))
+
+    # count the number of the specified letter between n and the point whetre you stopped multiplying ^^
+    remainder = s[ :(n % len(s)) ]
+
+    result += remainder.count(letter)
+
+    print(result)
+    return result
+
+if __name__ == '__main__':
+    s = 'kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm'
+    letter = 'a'
+    n = 736778906400
+    main(s, n)
